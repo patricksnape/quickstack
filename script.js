2f57cb61cff5f984bb5a5344b730e6dd62b19aa4
patricksnape <pts08@ic.ac.uk>
2012-10-19 17:12:20 +0100

    Expand the excerpt instead of a new div Also don't add an expando for one-line questions

--- script.js
+++ script.js
@@ -5,31 +5,35 @@ $('.summary > h3').each(function () {
 
     // Create a new question div who's class matches the summary
     // Append it after the summary and hide it
-    var question = document.createElement('div');
-    $(question).addClass($(summary).attr("class")).hide();
-    $(summary).after($(question));
+    var excerpt = $(this).siblings('.excerpt');
+    var full = $(excerpt).clone().html("").hide();
+    $(this).after(full);
 
-    // Create a button to expand and collapse the question body
-    var expando = document.createElement('span');
-    $(expando).html('&nbsp;&nbsp;&nbsp;&nbsp;').addClass('qscollapsed')
-        .click(function () {
-            if (!$(question).html()) {
-                $.get('https://api.stackexchange.com/2.1/questions/' + id + '?key=ccT8pcgxKLVpiCDOY2E9IQ((&order=desc&sort=activity&site=stackoverflow&filter=!)5E5HksQ.(saTK-ZSUzx*9N_1mst')
-                    .success(function (data) {
-                        $(question).html(data.items[0].body);
-                    })
-                    .complete(function () {
-                        toggleExpando(question, expando);
-                    });
-            } else {
-                toggleExpando(question, expando);
-            }
-        });
+    // Don't create an expando if it's a one line question
+    if ($(excerpt).text().indexOf("...") != -1) {
+        // Create a button to expand and collapse the question body
+        var expando = document.createElement('span');
+        $(expando).html('&nbsp;&nbsp;&nbsp;&nbsp;').addClass('qscollapsed')
+            .click(function () {
+                if (!$(full).html()) {
+                    $.get('https://api.stackexchange.com/2.1/questions/' + id + '?key=ccT8pcgxKLVpiCDOY2E9IQ((&order=desc&sort=activity&site=stackoverflow&filter=!)5E5HksQ.(saTK-ZSUzx*9N_1mst')
+                        .success(function (data) {
+                            $(full).html(data.items[0].body);
+                        })
+                        .complete(function () {
+                            toggleExpando(excerpt, full, expando);
+                        });
+                } else {
+                    toggleExpando(excerpt, full, expando);
+                }
+            });
 
-    $(this).append(expando);
+        $(this).append(expando);
+    }
 });
 
-function toggleExpando(question, expando) {
+function toggleExpando(excerpt, full, expando) {
     $(expando).toggleClass('qscollapsed').toggleClass('qsexpanded');
-    $(question).slideToggle(500);
+    $(excerpt).fadeToggle(500);
+    $(full).slideToggle(500);
 }
